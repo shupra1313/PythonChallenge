1d161deb12b3a66344c45bdb0e13c42d28aa46a3
Shu Pra <subha.prashanth@outlook.com>
2018-07-29 12:10:46 -0500

    save changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,33 @@
+# Importing dependency modules for os,csv & path
 import os
 import csv
+from pathlib import Path
+
+# Link file path to Python
+filepath = Path("PyBank","budget_data.csv")
+# Declare variables
+Total_Months = 0
+net_amount = 0
+Greatest_Increase = ("" (0))
+Greatest_Decrease = ("" (0))
+Average_Change = 0
+# open file which is linked from the path 
+with open(filepath, newline="") as csvfile:
+    # Read files
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # Loop through all the datas we collect 
+    for row in csvfile:
+        print(row)
+
+
+
+
+
+#Printing required final output
+#print("Financial Analysis")
+#print("----------------------------------")
+#print("Total Months:"    )
+#print("Total Net Amount:"   )
+#print("Average Change:"   )
+#print("Greatest Increase in Profits:"    )
+#print("Greatest Decrease in Profits:"    )
