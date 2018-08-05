3d4330e3a66118fae9bfe144e6526cf36fa69437
Shu Pra <subha.prashanth@outlook.com>
2018-08-05 17:05:58 -0500

    final update

---
+++ PyPoll/PyPoll.py
@@ -0,0 +1,44 @@
+import csv
+import os
+from pathlib import Path
+
+#set path
+filepath = Path("C:\\Users\\Subha\\repos\\python-challenge\\PyPoll\\election_data.csv")
+print('Election Results\n')
+print('---------------------------\n')
+#open the file
+with open(filepath, newline="",encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    #Initialize variables to empty lists and dictionary
+    votes = []
+    total_votes = 0
+    candidates = []
+    candidates_dict = {}
+    winning_votes = 0
+    next(csvreader)
+    #go line by line and process each vote
+    for row in csvreader:
+        #add to total number of votes
+        total_votes += 1
+        if row[2] not in candidates:
+            #add candidates to the empty list
+            candidates.append(row[2])
+        #to make candidate name as the key
+            candidates_dict[row[2]] = 0
+            candidates_dict[row[2]] = candidates_dict[row[2]] + 1
+        else:     
+            candidates_dict[row[2]] +=1
+    #print(candidates_dict)
+    for candidates in candidates_dict:
+        votes = candidates_dict.get(candidates)
+        percentage = (votes / total_votes)*100
+        print(str(candidates) + ':' + str(round(percentage,3)) + ("%") + '   (' + str(votes) + ')' '\n')
+        if votes > winning_votes:
+            winning_votes = votes
+            winner = candidates
+            
+print("Total Votes: " + str(total_votes) + '\n')
+print('----------------------------\n')
+print("Winner: " + str(winner) +'\n')
+print("Winning Votes: " + str(winning_votes) +'\n')
+print('----------------------------\n')
